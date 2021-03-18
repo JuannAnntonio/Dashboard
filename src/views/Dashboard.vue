b14671fe375363c219296a85c8410ca38944c28b
Jacob G. Acosta <jacob.gacosta@gmail.com>
2021-03-18 17:33:17 -0600

    Change graph by day

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -8,17 +8,17 @@
             <div class="wrap-input mb-3 mt-3">
               <select class="input" id="sucursal" v-model="sucursal">
                 <option
-                  v-bind:key="sucursal.id"
-                  v-for="sucursal in sucursales"
-                  :value="sucursal"
-                  >{{ sucursal.nombre }}</option
-                >
+                    v-bind:key="sucursal.id"
+                    v-for="sucursal in sucursales"
+                    :value="sucursal">
+                  {{ sucursal.nombre }}
+                </option>
               </select>
             </div>
             <template slot="footer">
               Aforo máx:
               <span class="text-primary">{{ sucursal.aforoMax }}</span>
-              <br />
+              <br/>
               Aforo:
               <span class="text-primary">{{ sucursal.aforoActual }}</span>
             </template>
@@ -26,18 +26,18 @@
         </div>
         <div class="col">
           <stats-card
-            title="Aforo actual"
-            type="gradient-primary"
-            :sub-title="porcentajeAforo + '%'"
-            icon="ni ni-chart-bar-32"
-            class="mb-2"
+              title="Aforo actual"
+              type="gradient-primary"
+              :sub-title="porcentajeAforo + '%'"
+              icon="ni ni-chart-bar-32"
+              class="mb-2"
           >
             <template slot="footer">
-              <br />
+              <br/>
               <base-progress
-                type="primary"
-                :height="8"
-                :value="porcentajeAforo"
+                  type="primary"
+                  :height="8"
+                  :value="porcentajeAforo"
               />
             </template>
           </stats-card>
@@ -53,10 +53,10 @@
                   <div class="col">
                     <base-input label="Inicio:">
                       <flat-picker
-                        placeholder="dd / mm / aaaa"
-                        class="form-control datepicker"
-                        :config="configEndDate"
-                        v-model="startDate"
+                          placeholder="dd / mm / aaaa"
+                          class="form-control datepicker"
+                          :config="configEndDate"
+                          v-model="startDate"
                       >
                       </flat-picker>
                     </base-input>
@@ -64,23 +64,23 @@
                   <div class="col">
                     <base-input label="Final:">
                       <flat-picker
-                        placeholder="dd / mm / aaaa"
-                        :config="configEndDate"
-                        class="form-control datepicker"
-                        v-model="endDate"
+                          placeholder="dd / mm / aaaa"
+                          :config="configEndDate"
+                          class="form-control datepicker"
+                          v-model="endDate"
                       >
                       </flat-picker>
                     </base-input>
                   </div>
                   <div class="col">
                     <div tabindex="-1" role="group" class="bv-no-focus-ring">
-                      <label class="form-control-label"><br /></label>
+                      <label class="form-control-label"><br/></label>
                       <div class="has-label">
                         <button
-                          type="submit"
-                          class="login-button"
-                          style="min-width: 15px; height: 40px;"
-                          @click="rango();"
+                            type="submit"
+                            class="login-button"
+                            style="min-width: 15px; height: 40px;"
+                            @click="range();"
                         >
                           Buscar
                         </button>
@@ -92,88 +92,88 @@
             </div>
             <div class="row justify-content-start">
               <div class="col-auto">
-                <h5 class="h3 text-primary mb-0">{{tituloGrafica }}</h5>
+                <h5 class="h3 text-primary mb-0">{{ tituloGrafica }}</h5>
               </div>
               <div class="col">
                 <b-nav class="nav-pills justify-content-end">
                   <b-nav-item
-                    :active="bigLineChart.activeIndex === 0"
-                    @click="
+                      :active="bigLineChart.activeIndex === 0"
+                      @click="
                       optionDate = false;
                       calculateForDay();
                     "
-                    link-classes="py-0 px-2"
+                      link-classes="py-0 px-2"
                   >
                     <span class="d-none d-md-block">Día</span>
                     <span class="d-md-none">D</span>
                   </b-nav-item>
                   <b-nav-item
-                    :active="bigLineChart.activeIndex === 1"
-                    @click="
+                      :active="bigLineChart.activeIndex === 1"
+                      @click="
                       optionDate = false;
                       calculateForWeek();
                     "
-                    link-classes="py-0 px-2"
+                      link-classes="py-0 px-2"
                   >
                     <span class="d-none d-md-block">Semana</span>
                     <span class="d-md-none">S</span>
                   </b-nav-item>
                   <b-nav-item
-                    @click="
+                      @click="
                       optionDate = false;
                       calculateForMonth();
                     "
-                    :active="bigLineChart.activeIndex === 2"
-                    link-classes="py-0 px-2"
+                      :active="bigLineChart.activeIndex === 2"
+                      link-classes="py-0 px-2"
                   >
                     <span class="d-none d-md-block">Mes</span>
                     <span class="d-md-none">M</span>
                   </b-nav-item>
                   <b-nav-item
-                    :active="bigLineChart.activeIndex === 3"
-                    @click="
+                      :active="bigLineChart.activeIndex === 3"
+                      @click="
                       optionDate = false;
                       calculateForYear();
                     "
-                    link-classes="py-0 px-2"
+                      link-classes="py-0 px-2"
                   >
                     <span class="d-none d-md-block">Año</span>
                     <span class="d-md-none">A</span>
                   </b-nav-item>
                   <b-nav-item
-                    :active="bigLineChart.activeIndex === 4"
-                    @click="
+                      :active="bigLineChart.activeIndex === 4"
+                      @click="
                       optionDate = true;
                       bigLineChart.activeIndex = 4;
                     "
-                    link-classes="py-0 px-2"
+                      link-classes="py-0 px-2"
                   >
                     <span class="d-none d-md-block">
                       <el-tooltip
-                        placement="bottom"
-                        content="Fecha Personalizada"
+                          placement="bottom"
+                          content="Fecha Personalizada"
                       >
-                        <i class="ni ni-calendar-grid-58" />
+                        <i class="ni ni-calendar-grid-58"/>
                       </el-tooltip>
                     </span>
                     <span class="d-md-none"
-                      ><el-tooltip
+                    ><el-tooltip
                         placement="bottom"
                         content="Fecha Personalizada"
-                      >
-                        <i class="ni ni-calendar-grid-58" /> </el-tooltip
+                    >
+                        <i class="ni ni-calendar-grid-58"/> </el-tooltip
                     ></span>
                   </b-nav-item>
                 </b-nav>
               </div>
             </div>
-            <br />
+            <br/>
             <div class="row justify-content-start">
               <div class="col">
                 <line-chart
-                  :height="350"
-                  :chart-data="bigLineChart.chartData"
-                  :extra-options="bigLineChart.extraOptions"
+                    :height="350"
+                    :chart-data="bigLineChart.chartData"
+                    :extra-options="bigLineChart.extraOptions"
                 >
                 </line-chart>
               </div>
@@ -181,19 +181,19 @@
           </div>
           <div class="col-xl-5 col-md-12 col-sm-12">
             <h5 class="h3 text-primary mb-0" style="text-align: center;">Visitantes</h5>
-            <br />
+            <br/>
             <dona-chart
-              :height="300"
-              :chart-data="chartDonaData"
-              :options="optionsPieData"
+                :height="300"
+                :chart-data="chartDonaData"
+                :options="optionsPieData"
             />
           </div>
         </div>
-        <br />
+        <br/>
         <div class="row justify-content-start">
           <div class="col">
             <h5 class="h3 text-primary mb-0" style="text-align: center;">Mapa de calor</h5>
-            <img src="img/svg/MapadeCalorProp1.svg" />
+            <img src="img/svg/MapadeCalorProp1.svg"/>
           </div>
         </div>
       </div>
@@ -219,7 +219,7 @@ export default {
     return {
       optionDate: false,
       datos,
-      tituloGrafica:'',
+      tituloGrafica: '',
       labelX: [],
       labelY: [],
       bigLineChart: {
@@ -253,12 +253,12 @@ export default {
         nombre: "Plaza Virtual",
         aforoMax: 0,
         aforoActual: 0,
-        niños: 0,
+        ninios: 0,
         adultos: 0,
       },
       sucursales: [
-        { id: 1, nombre: "Plaza Virtual" },
-        { id: 2, nombre: "Plaza Madero" },
+        {id: 1, nombre: "Plaza Virtual"},
+        {id: 2, nombre: "Plaza Madero"},
       ],
       dataPastel: [],
       optionsPieData: {
@@ -287,7 +287,7 @@ export default {
       },
 
       porcentajeAforo: "0",
-      startDate: "",
+      startDate: "2021-03-01",
       endDate: "",
       maxDate: "",
       configEndDate: {
@@ -298,65 +298,102 @@ export default {
     };
   },
   methods: {
-    rango(){
+    range() {
+      let initDate = new Date(this.startDate);
+      let endDate = new Date(this.endDate);
 
-      let ini = new Date(this.startDate);
-      let end = new Date(this.endDate);
+      let diferenciaDias = endDate.getTime() - initDate.getTime()
+      console.log(Math.round(diferenciaDias / (1000 * 60 * 60 * 24)))
 
-      let diferenciaDias = end.getTime() - ini.getTime()
-      console.log(Math.round(diferenciaDias/ (1000*60*60*24)))
-
-      if(diferenciaDias==0){
+      if (diferenciaDias == 0) {
         this.calculateForDay();
-      } else if(diferenciaDias<=7){
+      } else if (diferenciaDias <= 7) {
         this.calculateForWeek();
-      } else if(diferenciaDias>=32){
+      } else if (diferenciaDias >= 32) {
         this.calculateForMonth();
-      } else if(diferenciaDias>=366){
+      } else if (diferenciaDias >= 366) {
         this.calculateForYear();
       }
     },
-    calculateForDay(){
+    calculateForDay() {
+      this.bigLineChart.activeIndex = 0;
       this.tituloGrafica = "Resumen actual"
-      this.initBigChart(0);  
+      this.initBigChart(4);
     },
-    calculateForWeek(){
+    calculateForWeek() {
+      this.bigLineChart.activeIndex = 1;
       this.tituloGrafica = "Resumen semanal"
-      this.initBigChart(1);  
+      this.initBigChart(1);
     },
-    calculateForMonth(){
+    calculateForMonth() {
+      this.bigLineChart.activeIndex = 2;
       this.tituloGrafica = "Resumen mensual"
-      this.initBigChart(2);  
+      this.initBigChart(5);
     },
-    calculateForYear(){
+    calculateForYear() {
+      this.bigLineChart.activeIndex = 3;
       this.tituloGrafica = "Resumen anual"
-      this.initBigChart(3);  
+      this.initBigChart(3);
     },
     initBigChart(index) {
-
-      var labelX = [];
-      var labelY = [];
+      const labelX = [];
+      const labelY = [];
       this.bigLineChart.chartData = {};
 
-      //console.log(this.datos[index].aforo);
-      var niños = 0;
-      var adultos = 0;
-      var aforoMax = 0;
-      var sucursal = "";
-      this.datos[index].aforo.forEach(function(element) {
-        labelX.push(element.grafica.x);
-        labelY.push(element.grafica.y);
-        aforoMax = element.aforoMax;
-        sucursal = element.sucursal;
-        niños += element.niño;
-        adultos += element.adulto;
-      });
+      let ninios = 0;
+      let adultos = 0;
+      let aforoMax = 0;
+      let sucursal = "";
 
-      this.sucursal.aforoMax = aforoMax;
+      const self = this;
+
+      if (index === 4) {
+        this.datos[index].aforo.forEach(function (element) {
+          const date = element.fecha.substring(0, 4) + "-" + element.fecha.substring(4, 6) + "-" + element.fecha.substring(6);
+
+          if (date === self.startDate) {
+            labelX.push(element.x);
+            labelY.push(parseInt(element.y));
+            aforoMax += parseInt(element.aforoMax);
+            sucursal = element.sucursal;
+            ninios += parseInt(element.niño);
+            adultos += parseInt(element.adulto);
+          }
+        });
+      } else if (index === 5) {
+        this.datos[index].aforo.forEach(function (element) {
+          const month = element.fecha.substring(4, 6);
+
+          if (month === "03") {
+            labelX.push(element.x);
+            labelY.push(parseInt(element.y));
+            aforoMax += parseInt(element.aforoMax);
+            sucursal = element.sucursal;
+            ninios += parseInt(element.niño);
+            adultos += parseInt(element.adulto);
+          }
+        });
+      } else if (index === 1) {
+        this.datos[5].aforo.forEach(function (element) {
+          const day = element.fecha.substring(6);
+          const month = element.fecha.substring(4, 6);
+
+          if (parseInt(day) < 8 && month === "03") {
+            labelX.push(element.x);
+            labelY.push(parseInt(element.y));
+            aforoMax += parseInt(element.aforoMax);
+            sucursal = element.sucursal;
+            ninios += parseInt(element.niño);
+            adultos += parseInt(element.adulto);
+          }
+        });
+      }
+
       this.sucursal.nombre = sucursal;
-      this.sucursal.niños = niños;
+      this.sucursal.ninios = ninios;
       this.sucursal.adultos = adultos;
-      this.sucursal.aforoActual = Math.trunc((niños + adultos) / 5);
+      this.sucursal.aforoMax = aforoMax;
+      this.sucursal.aforoActual = Math.trunc((ninios + adultos) / 5);
       this.labelX = labelX;
       this.labelY = labelY;
 
@@ -364,7 +401,7 @@ export default {
         labels: ["Adultos", "Niños"],
         datasets: [
           {
-            data: [adultos, niños],
+            data: [adultos, ninios],
             backgroundColor: ["#5e72e4", "#9ba8f49c "],
             borderColor: "black",
             hoverBorderColor: "black",
@@ -385,7 +422,6 @@ export default {
         labels: this.labelX,
       };
       this.bigLineChart.chartData = chartData;
-      this.bigLineChart.activeIndex = index;
 
       this.calculaPorcentajeAforo();
     },
@@ -393,12 +429,12 @@ export default {
       //console.log(this.sucursal.aforoActual);
       //console.log(this.sucursal.aforoMax);
       this.porcentajeAforo =
-        (this.sucursal.aforoActual / this.sucursal.aforoMax) * 100;
+          (this.sucursal.aforoActual / this.sucursal.aforoMax) * 100;
       //console.log(this.porcentajeAforo);
     },
   },
   created() {
-    this.initBigChart(3);
+    this.calculateForDay();
   },
 };
 </script>
@@ -406,34 +442,43 @@ export default {
 .container {
   max-width: 95% !important;
 }
+
 .card {
   width: 100%;
 }
+
 .text-primary {
   font-weight: bolder;
   color: var(--primary) !important;
 }
+
 .bg-gradient-primary {
   background: var(--primary) !important;
 }
+
 .bg-primary {
   background-color: var(--primary) !important;
 }
+
 .nav-pills .nav-link.active,
 .nav-pills .show > .nav-link {
   color: var(--secondary) !important;
   background-color: var(--primary) !important;
 }
+
 .nav-pills .nav-link {
   color: var(--primary) !important;
 }
+
 .row {
   padding-bottom: 2px;
   padding-top: 2px;
 }
+
 .col-md-6 {
   max-width: 30% !important;
 }
+
 .flatpickr-input[readonly],
 .form-control[readonly] {
   background-color: #fff;
